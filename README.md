ed76121c2a2d2ac372c1388711bad607953a898e
Rafhael-Josino <rafhael_josino1@hotmail.com>
2023-06-29 13:10:12 -0300

    Function that adds elements to a zabbix map

--- README.md
+++ README.md
@@ -8,8 +8,4 @@ This project consists in some solutions that aim to automate tasks from differen
 
 # TO DO
 
-- To the firewall environment, create a script to discover the address which have the same subnet. In the Fortigate, an address entity can be associated to a subnet (from a single IP to any network size), as it can also translate to the real world as a MAC address or a domain name. Therefore, it is possible to hava multiple address objects with the same subnets (just imagine people who do not talk to each other meddling with the firewall and creating the same networks in different objects).
-
-- Extend the project to a Zabbix server using py-zabbix.
-
 - In the network environment, create playbooks with Dell modules.
---
+++ zabbix/add_map_elements.py
@@ -0,0 +1,65 @@
+'''
+Adds elements in a map with the hosts passed in a box whose
+dimentions are also passed as parameters
+
+- (x0,y0) is the initial point where the first element will be inserted
+- width represents the multiple of spacement that will constitute the actual
+width of the box.
+	For example, a width of 3 means that the box will have 3 elements of width
+- height is the same thing but in the other dimension. This parameter and width
+are both mutually excludents
+'''
+
+def add_element(zapi, sysmapid, hosts, iconid, x0, y0, spacement, width=None, height=None):
+	limit = (width or height)
+	if limit == None or limit == 0:
+		print("Error with the limiting dimension")
+		print("Width received:", width)
+		print("Higth received:", height)
+		return None
+
+	# Gets Zabbix map
+	this_map = zapi.map.get(
+		sysmapids=sysmapid,
+		output="extend",
+		selectSelements="extend",
+		selectLinks="extend"
+	)
+
+	# Gets map's elements
+	selements = this_map[0]["selements"]
+
+	x = 0
+	y = 0
+	counter = 0
+
+	print('limit:', limit)
+	print("(x0,y0): ({},{})".format(x0,y0))
+
+	# Creates the new elements
+	for host in hosts:
+		new_selement = {
+			"elements": [host],
+			"elementtype": 0,
+			"label": host["name"][7:],
+			"iconid_off": iconid,
+			"iconid_on": iconid,
+			"sysmapid": sysmapid,
+			"x": x + x0,
+			"y": y + y0,
+		}
+
+		selements.append(new_selement)
+
+		# Update next position
+		# Fulling by line
+		if width:
+			counter += 1
+			x = (counter % limit) * spacement
+			y = (counter // limit) * spacement
+
+		# Make fulling by column
+		elif column:
+			pass
+
+	zapi.map.update(sysmapid=sysmapid, selements=selements)
