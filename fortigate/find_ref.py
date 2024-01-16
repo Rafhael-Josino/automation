407d65571cd95bf247d0eedfddf44c35c27055bb
Rafhael-Josino <rafhael_josino1@hotmail.com>
2024-01-16 11:43:42 -0300

    Find address references update

--- fortigate/find_ref.py
+++ fortigate/find_ref.py
@@ -2,37 +2,17 @@ from colorama import init
 from termcolor import colored
 
 
-def find_ref(fgt, ip, mask=None):
-	print(100*'-', "\n")
-	init()
-	subnet = ip + " " + (mask if mask else "255.255.255.255")
-	
-	address_search = fgt.address.get(filter = ("subnet=@" + subnet))
-
-	if len(address_search) == 0:
-		print("There is no addresses with this subnet")
-		return None
-
-
-	if len(address_search) > 1:
-		print(colored("There are {} repeated addresses with the same subnet".format(len(address)), "red"))
-
-		i=1
-		for address in address_search:
-			print("{} > {}".format(i, address["name"]))
-		
-		return None
-
-
-	print("One address found:", end=" ")
-	print(colored(address_search[0]["name"]+"\n", "green"))
+def detail_refs(fgt, name):
+	print("Address found:", end=" ")
+	print(colored(name + "\n", "green"))
 
 	print(100*'-', "\n")
 
-	groups = fgt.address_group.get(filter="member=@"+address_search[0]["name"])
+	# Check if address is present in any group
+	groups = fgt.address_group.get(filter="member=@" + name)
 	
-	print("Found address in", end=" ")
-	print(colored(str(len(groups)) + " groups\n", "green"))
+	print("Address found in:", end=" ")
+	print(colored(str(len(groups)) + " grupos\n", "green"))
 
 	for group in groups:
 		print('>' + group["name"])
@@ -40,9 +20,9 @@ def find_ref(fgt, ip, mask=None):
 	print(100*'-', "\n")
 
 	# Check if address is a source of any policies
-	src_policies = fgt.policy.get(filter="srcaddr=@"+address_search[0]["name"])
+	src_policies = fgt.policy.get(filter = "srcaddr=@" + name)
 
-	print("Found address in the source of", end=" ")
+	print("Address found in the origin of", end=" ")
 	print(colored(str(len(src_policies)) + " policies\n", "green"))
 
 	for policy in src_policies:
@@ -51,12 +31,50 @@ def find_ref(fgt, ip, mask=None):
 	print(100*'-', "\n")
 
 	# Check if address is the destiny of any policies
-	dst_policies = fgt.policy.get(filter="dstaddr=@"+address_search[0]["name"])
+	dst_policies = fgt.policy.get(filter = "dstaddr=@" + name)
 
-	print("Found address in the destiny of", end=" ")
+	print("Address found in the destiny of", end=" ")
 	print(colored(str(len(dst_policies)) + " policies\n", "green"))
 
 	for policy in dst_policies:
 		print('>' + policy["name"])
 	
 	print(100*'-', "\n")
+
+
+def find_ref(fgt, ip, mask=None):
+	print(100*'-', "\n")
+	init()
+	subnet = ip + " " + (mask if mask else "255.255.255.255")
+	
+	address_search = fgt.address.get(filter = ("subnet=@" + subnet))
+
+	if len(address_search) == 0:
+		print("There are no address with this IP")
+		return None
+
+
+	if len(address_search) > 1:
+		print(colored("There are {} addresses with the same subnet".format(len(address_search)), "red"))
+
+		loop = True
+		while loop:
+			i=1
+			for address in address_search:
+				print("{} > {}".format(i, address["name"]))
+				i += 1
+		
+			print(i, "- LEAVE")
+
+			option = int(input("Option: "))
+			
+			if option < i and option:
+				detail_refs(fgt, address_search[option-1]["name"]) 
+
+			else:
+				loop = False
+
+		print(100*'-', "\n")
+		return None
+
+	detail_refs(fgt, address_search[0]["name"])
